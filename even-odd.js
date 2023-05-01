00958f95e0cf9c9c9b15ace87bffaf4c3fb3434c
pradeep <pradeeprakh2001@gmail.com>
2023-05-02 01:52:35 +0530

    ADDTWONUM

---
+++ even-odd.js
@@ -0,0 +1,10 @@
+const prompt=require('prompt-sync')()
+num = parseInt(prompt("Enter a number: "))
+if(num % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
